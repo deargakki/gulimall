6e1a94f284d736732967ceed192919ed55441f8b
gakki <htyszuedu@gmail.com>
2024-01-08 00:35:48 +0800

    p76

--- guli-product/src/test/java/com/atguigu/gulimall/product/GuliProductApplicationTests.java
+++ guli-product/src/test/java/com/atguigu/gulimall/product/GuliProductApplicationTests.java
@@ -2,15 +2,35 @@ package com.atguigu.gulimall.product;
 
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
+import com.atguigu.gulimall.product.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+
+@Slf4j
 @SpringBootTest
 class GuliProductApplicationTests {
 
     @Autowired
     BrandService brandService;
+
+    @Autowired
+    CategoryService categoryService;
+
+    @Test
+    void testPath(){
+        Long[] path = categoryService.findCatelogPath(225L);
+        log.info("完整路径：{}", Arrays.asList(path));
+    }
+//    @Autowired
+//    OSSClient ossClient;
     @Test
     void contextLoads() {
         BrandEntity brandEntity = new BrandEntity();
@@ -18,5 +38,17 @@ class GuliProductApplicationTests {
         brandService.save(brandEntity);
         System.out.println("保存成功");
     }
+
+
+//    @Test
+//    public void testUpload() throws FileNotFoundException{
+//        InputStream inputStream = new FileInputStream("C:\\Users\\ga'k'k'i\\OneDrive\\图片\\屏幕快照\\2022-04-27 (1).png");
+//
+//        ossClient.putObject("gulimall-gakkilove","1.png",inputStream);
+//
+//        ossClient.shutdown();
+//
+//        System.out.println("上传完成。。。。");
+//    }
 
 }
---
+++ guli-third-party/src/main/java/com/atguigu/gulimall/thirdparty/controller/OssController.java
@@ -0,0 +1,80 @@
+package com.atguigu.gulimall.thirdparty.controller;
+
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.common.auth.CredentialsProviderFactory;
+import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
+import com.atguigu.common.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+public class OssController {
+
+    @Autowired
+    OSS oss;
+
+    @Value("${spring.cloud.alicloud.oss.endpoint}")
+    private String endpoint;
+
+    private static String bucket = "gulimall-gakkilove";
+
+    @Value("${spring.cloud.alicloud.access-key}")
+    private String accessId;
+
+    @RequestMapping("/oss/policy")
+    public R policy(){
+        String bucket = "gulimall-gakkilove";
+        // 填写Host名称，格式为https://bucketname.endpoint。
+        String host = "https://gulimall-gakkilove.oss-cn-beijing.aliyuncs.com";
+        // 设置上传回调URL，即回调服务器地址，用于处理应用服务器与OSS之间的通信。OSS会在文件上传完成后，把文件上传信息通过此回调URL发送给应用服务器。
+        String callbackUrl = "https://192.168.0.0:8888";
+        // 设置上传到OSS文件的前缀，可置空此项。置空后，文件将上传至Bucket的根目录下。
+        String format = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String dir = format+"/";
+        Map<String, String> respMap = new LinkedHashMap<String, String>();
+        try {
+            long expireTime = 30;
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            // PostObject请求最大可支持的文件大小为5 GB，即CONTENT_LENGTH_RANGE为5*1024*1024*1024。
+            PolicyConditions policyConds = new PolicyConditions();
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = oss.generatePostPolicy(expiration, policyConds);
+            byte[] binaryData = postPolicy.getBytes("utf-8");
+            String accessId = this.accessId;
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = oss.calculatePostSignature(postPolicy);
+
+            respMap.put("accessid", accessId);
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            respMap.put("dir", dir);
+            respMap.put("host", host);
+            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+            // respMap.put("expire", formatISO8601Date(expiration));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally{
+            oss.shutdown();
+        }
+        return R.ok().put("data",respMap);
+    }
+
+}
+
